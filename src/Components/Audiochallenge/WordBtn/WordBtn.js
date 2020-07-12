19e55ee7d5fefad666aa7d72599782d1ac4e783c
kli2m <ivanklimm87@gmail.com>
2020-07-12 22:50:20 +0300

    fix: eslint fixed

--- src/Components/Audiochallenge/WordBtn/WordBtn.js
+++ src/Components/Audiochallenge/WordBtn/WordBtn.js
@@ -8,9 +8,10 @@ const WordBtn = (props) => {
   const { words, isChosed, verificationWord } = props;
 
   const hundlerKeypress = (e) => {
-    if ( !isChosed.isChosed) 
-    for (let i = 1; i < 6; i += 1) {
-      if (e.key === `${i}`) verificationWord(e, document.querySelector(`.btn${i}`).value);
+    if (!isChosed.isChosed) {
+      for (let i = 1; i < 6; i += 1) {
+        if (e.key === `${i}`) verificationWord(e, document.querySelector(`.btn${i}`).value);
+      }
     }
   };
 
